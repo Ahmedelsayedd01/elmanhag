a0f378816413e4265df7be018799e7cfe5cc5e1d
Almatador <74638629+almatador@users.noreply.github.com>
2024-08-31 15:00:58 +0300

    Create signuppage.jsx

---
+++ src/Pages/Student/loginpage/signuppage.jsx
@@ -0,0 +1,14 @@
+// src/pages/SignupPage.jsx
+import React from 'react';
+import Layout from './../../../Layouts/Student/loginlayout';
+import SignupForm from './../../../Layouts/Student/SignupForm';
+
+const SignupPage = () => {
+  return (
+    <Layout>
+      <SignupForm />
+    </Layout>
+  );
+};
+
+export default SignupPage;
