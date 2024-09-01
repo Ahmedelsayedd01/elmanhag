9fe793f7324b17096d20f13df128fc514cb5c5af
Basantsaeed <89366449+Basantsaed@users.noreply.github.com>
2024-09-01 11:51:08 +0300

    fcommit

--- src/Layouts/Admin/AddHomeWorkLayout.jsx
+++ src/Layouts/Admin/AddHomeWorkLayout.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import HeaderPageSection from '../../Components/HeaderPageSection';
+import { AddHomeWorkPage } from '../../Pages/AllPages';
 
 const AddHomeWorkLayout = () => {
+  const navigate = useNavigate();
+      const handleGoBack = () => {
+              navigate(-1, { replace: true });
+      };
+
   return (
-    <div>AddHomeWorkLayout</div>
-  )
+        <>
+              <HeaderPageSection handleClick={handleGoBack} name="Add H.W" />
+              <AddHomeWorkPage />
+        </>
+          )
 }
 
 export default AddHomeWorkLayout
+
