460f29b1378cb279fc7deb0ffe6e100978d590f2
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-09-02 16:16:44 +0300

    new Update A

---
+++ src/Layouts/Admin/MaterialLessonLayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import HeaderPageSection from '../../Components/HeaderPageSection';
+import { MaterialLessonPage } from '../../Pages/AllPages';
+
+const MaterialLessonLayout = () => {
+       const navigate = useNavigate();
+       const handleGoBack = () => {
+              navigate(-1, { replace: true });
+       };
+       return (
+              <>
+                     <HeaderPageSection handleClick={handleGoBack} name="Material Lesson" />
+                     <MaterialLessonPage />
+              </>
+       )
+}
+
+export default MaterialLessonLayout
