21e984bb4d997e152f929b7df1f891372c0707ca
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-09-02 16:17:23 +0300

    new Update A

--- src/Pages/RegisterPage/Authentication.jsx
+++ src/Pages/RegisterPage/Authentication.jsx
@@ -4,8 +4,9 @@ import Logo from "../../Assets/Images/logoBlack"
 import TextTitle from '../../Components/TextTitle';
 
 const Authentication = () => {
-       const [isActiveLogin, setIsActiveLogin] = useState(true);
-       const [isActiveSignUP, setIsActiveSignUP] = useState(false);
+       const [isActiveLogin, setIsActiveLogin] = useState(false);
+       const [isActiveSignUP, setIsActiveSignUP] = useState(true);
+       const [isActiveAffilliate, setIsActiveAffilliate] = useState(false);
 
        const handleClickLogin = () => {
               setIsActiveLogin(!isActiveLogin);
@@ -15,6 +16,11 @@ const Authentication = () => {
               setIsActiveSignUP(!isActiveSignUP);
               setIsActiveLogin(false);
        };
+       const handleClickAffilliate = () => {
+              setIsActiveSignUP(false);
+              setIsActiveLogin(false);
+              setIsActiveAffilliate(true);
+       };
 
        return (
               <>
@@ -23,11 +29,16 @@ const Authentication = () => {
                             <div className="flex items-center justify-center w-full lg:w-6/12 h-full">
                                    <div className="flex flex-col items-center justify-start h-5/6 gap-8 w-10/12">
                                           <TextTitle text={'Hello guys!'} color={'mainColor'} font={'medium'} />
-                                          <nav className="flex items-center w-full justify-center gap-8 mt-4">
-                                                 <NavLink to={"signup"} className={isActiveSignUP ? "text-2xl font-medium text-secoundColor px-6 py-3 bg-mainColor rounded-xl" : "text-2xl font-medium px-6 py-3 text-mainColor"} onClick={handleClickSignUp}>Sing up</NavLink>
-                                                 <NavLink to={"login"} className={isActiveLogin ? "text-2xl font-medium text-secoundColor px-6 py-3 bg-mainColor rounded-xl" : "text-2xl font-medium px-6 py-3 text-mainColor"} onClick={handleClickLogin}>Log in</NavLink>
-                                          </nav>
+                                          {isActiveAffilliate ?
+                                                 ('') : (
+
+                                                        <nav className="flex items-center w-full justify-center gap-8 mt-4">
+                                                               <NavLink to={"signup"} className={isActiveSignUP ? "text-2xl font-medium text-secoundColor px-6 py-3 bg-mainColor rounded-xl" : "text-2xl font-medium px-6 py-3 text-mainColor"} onClick={handleClickSignUp}>Sing up</NavLink>
+                                                               <NavLink to={"login"} className={isActiveLogin ? "text-2xl font-medium text-secoundColor px-6 py-3 bg-mainColor rounded-xl" : "text-2xl font-medium px-6 py-3 text-mainColor"} onClick={handleClickLogin}>Log in</NavLink>
+                                                        </nav>
+                                                 )}
                                           <Outlet />
+                                          <NavLink to={"signup_affiliate"} className={isActiveAffilliate ? "hidden text-2xl font-medium text-secoundColor px-6 py-3 bg-mainColor rounded-xl" : "text-2xl font-medium px-6 py-3 text-mainColor"} onClick={handleClickAffilliate}>Sign As Affilliate</NavLink>
                                    </div>
                             </div>
                             <div className="hidden lg:flex items-center justify-center w-6/12">
---
+++ src/Pages/RegisterPage/SignUpAffiliatePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUpAffiliatePage = () => {
+  return (
+    <div>SignUpAffiliatePage</div>
+  )
+}
+
+export default SignUpAffiliatePage
