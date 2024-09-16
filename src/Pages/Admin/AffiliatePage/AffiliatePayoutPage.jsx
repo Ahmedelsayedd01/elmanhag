5f3821bbebcd175973c5554f289dd9678a9868d4
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-09-16 15:03:21 +0300

    New Update A

--- src/Pages/Admin/AffiliatePage/AffiliatePayoutPage.jsx
+++ src/Pages/Admin/AffiliatePage/AffiliatePayoutPage.jsx
@@ -1,19 +1,34 @@
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { NavLink, useLocation, useNavigate } from 'react-router-dom'
 
 const AffiliatePayoutPage = () => {
+       const location = useLocation()
+       const navigate = useNavigate()
+       const [path, setPath] = useState(location.pathname)
        const [isActivePending, setIsActivePending] = useState(true)
        const [isActiveHistory, setIsActiveHistory] = useState(false)
+       // const path = location.pathname;
+       console.log('locationsss', path)
 
        const handleClickPending = () => {
               setIsActivePending(true)
               setIsActiveHistory(false)
+              navigate('pending')
        }
        const handleClickHistory = () => {
               setIsActiveHistory(true)
               setIsActivePending(false)
-
+              navigate('history')
        }
+       useEffect(() => {
+
+              if (path === '/dashboard_admin/affiliate_payout/pending' || path === '/dashboard_admin/affiliate_payout/' || path === '/dashboard_admin/affiliate_payout') {
+                     handleClickPending()
+              }
+              if (path === '/dashboard_admin/affiliate_payout/history') {
+                     handleClickHistory()
+              }
+       }, [])
        return (
               <>
                      <nav className="flex items-center w-full justify-center gap-8 mt-4">
