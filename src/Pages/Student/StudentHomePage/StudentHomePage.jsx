d83f2a59f0de61fdbcf858ac6b91980df331334b
Almatador <74638629+almatador@users.noreply.github.com>
2024-09-18 12:02:51 +0300

    ww

--- src/Pages/Student/StudentHomePage/StudentHomePage.jsx
+++ src/Pages/Student/StudentHomePage/StudentHomePage.jsx
@@ -1,18 +1,62 @@
-import React from 'react'
-import {CurriculaIcon} from '../../../Components/Icons/StudentIcons/CurriculaIcon'
-const StudentHomePage = () => {
-       return (
-              <div>
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import CurriculaIcon from '../../../Components/Icons/StudentIcons/CurriculaIcon'
+import DutiesIcon from '../../../Components/Icons/StudentIcons/DutiesIcon'
+import MonthsReviewsIcon from '../../../Components/Icons/StudentIcons/MonthsReviewsIcon'
+import SolveExamsIcon from '../../../Components/Icons/StudentIcons/SolveExamsIcon'
 
-              <div className={`w-64 min-h-36 overflow-hidden p-3 flex flex-col items-center justify-start bg-mainColor rounded-xl`}>
-                     <span className='w-full text-center text-xl text-secoundColor font-medium'>
-                            <CurriculaIcon/>
+const StudentHomePage = () => {
+       const [isActiveCurricula, setIsActiveCurricula] = useState(false);
+       const [isActiveDuties, setIsActiveDuties] = useState(false);
+       const [isActiveMonthsReviews, setIsActiveMonthsReviews] = useState(false);
+       const [isActiveSolveExams, setIsActiveSolveExams] = useState(false);
+
+       return (
+              <div className="flex gap-5 p-4 flex-wrap justify-center">
+
+              <div className={`w-56 min-h-36 overflow-hidden p-3 flex flex-col items-center justify-start bg-mainColor rounded-xl`}>
+              <NavLink to="curricula">
+                     <span className='w-full flex justify-center text-xl text-secoundColor font-medium'>
+                            <CurriculaIcon isActive={isActiveCurricula} Width="64" Height="64"/>
                      </span>
-                     <span className='w-full mt-6 text-center text-5xl text-secoundColor font-medium'>
-                            count
+                     <span className='w-full mt-3 text-center text-5xl text-secoundColor font-medium'>
+                            المناهج
                      </span>
+              </NavLink>
               </div>
-                     StudentHomePage
+                     
+              <div className={`w-56 min-h-36 overflow-hidden p-3 flex flex-col items-center justify-start bg-mainColor rounded-xl`}>
+              <NavLink to="duties">
+                     <span className='w-full flex justify-center text-xl text-secoundColor font-medium'>
+                            <DutiesIcon isActive={isActiveDuties} Width="64" Height="64"/>
+                     </span>
+                     <span className='w-full mt-3 text-center text-5xl text-secoundColor font-medium'>
+                            الواجبات
+                     </span>
+              </NavLink>
+              </div>
+
+              <div className={`w-56 min-h-36 overflow-hidden p-3 flex flex-col items-center justify-start bg-mainColor rounded-xl`}>
+              <NavLink to="months_reviews">
+                     <span className='w-full flex justify-center text-xl text-secoundColor font-medium'>
+                            <MonthsReviewsIcon isActive={isActiveMonthsReviews} Width="64" Height="64"/>
+                     </span>
+                     <span className='w-full mt-3 text-center text-5xl text-secoundColor font-medium'>
+                            المراجعات
+                     </span>
+              </NavLink>
+              </div>
+
+              <div className={`w-56 min-h-36 overflow-hidden p-3 flex flex-col items-center justify-start bg-mainColor rounded-xl`}>
+              <NavLink to="solve_exams">
+                     <span className='w-full flex justify-center text-xl text-secoundColor font-medium'>
+                            <SolveExamsIcon isActive={isActiveSolveExams} Width="64" Height="64"/>
+                     </span>
+                     <span className='w-full mt-3 text-center text-5xl text-secoundColor font-medium'>
+                            الامتحانات
+                     </span>
+              </NavLink>
+              </div>
               </div>
        )
 }
