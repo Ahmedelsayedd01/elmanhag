4044d7f161d7b3b38749ae56e568460ddd1b922d
Almatador <74638629+almatador@users.noreply.github.com>
2024-09-18 10:25:24 +0300

    fcommit

--- src/Components/NavbarStudent.jsx
+++ src/Components/NavbarStudent.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { IoSearch } from 'react-icons/io5';
 import { Button } from "./Button";
 import { useAuth } from "../Context/Auth";
+import { Link, useNavigate } from 'react-router-dom'
 
 const NavbarStudent = () => {
     const auth = useAuth();
@@ -11,13 +12,29 @@ const NavbarStudent = () => {
         setUserName(auth.user?.name || '');
     }, [auth.user]);
     console.log('userName', userName)
+
+    const navigate = useNavigate();
+
+       const handleLogout = () => {
+              auth.logout();
+              navigate("/authentication/login", { replace: true });
+       }
+       const handleGoBack = () => {
+              navigate(-1, { replace: true });
+              localStorage.removeItem("sidebarState")
+       }
     return (
         <main className="bg-white p-4 flex flex-col items-center">
             <div className="flex items-center justify-between w-full max-w-6xl">
-                <div className="flex flex-col items-start mr-8">
+                <div className="flex w-full justify-between mr-8">
+                    <div>
                     <h3 className="text-red-500 bg-white p-2 rounded-md mb-2 text-2xl font-bold">
                         مرحباً بك {userName}
                     </h3>
+                    </div>
+                    <div>
+                    <button type='button' className="px-4 py-3 mx-auto text-2xl  text-secoundColor bg-mainColor rounded-2xl" onClick={handleLogout}>Log Out</button>
+                    </div>
                 </div>
                 <div className="flex items-center">
                     <div className="ml-4">
--- src/Pages/Student/StudentHomePage/StudentHomePage.jsx
+++ src/Pages/Student/StudentHomePage/StudentHomePage.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import {CurriculaIcon} from '../../../Components/Icons/StudentIcons/CurriculaIcon'
 const StudentHomePage = () => {
        return (
               <div>
+
+              <div className={`w-64 min-h-36 overflow-hidden p-3 flex flex-col items-center justify-start bg-mainColor rounded-xl`}>
+                     <span className='w-full text-center text-xl text-secoundColor font-medium'>
+                            <CurriculaIcon/>
+                     </span>
+                     <span className='w-full mt-6 text-center text-5xl text-secoundColor font-medium'>
+                            count
+                     </span>
+              </div>
                      StudentHomePage
               </div>
        )
