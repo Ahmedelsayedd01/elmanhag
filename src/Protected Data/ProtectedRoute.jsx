8b5e7969b6032b1be29751988522239fb07075ed
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-09-24 14:58:38 +0300

    back to the main page elmanhag.com

--- src/Protected Data/ProtectedRoute.jsx
+++ src/Protected Data/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { useAuth } from '../Context/Auth';
 
 const ProtectedRoute = ({ allowedRoles }) => {
-       
+
        const location = useLocation();
        const auth = useAuth();
 
@@ -18,10 +18,14 @@ const ProtectedRoute = ({ allowedRoles }) => {
               .map(role => role.toLowerCase())
               .some(role => userRoles.includes(role));
        console.log("userRoles", userRoles)
+       console.log("hasPermission", hasPermission)
 
        if (!hasPermission) {
-              return <Navigate to="/unauthorized" state={{ from: location }} replace />;
+              return window.location.href = "https://elmanhag.com/";
        }
+       // if (location.pathname == 'login.elmanhag.shop') {
+       //        return window.location.href = "https://elmanhag.com/";
+       // }
 
        return <Outlet />;
 };
