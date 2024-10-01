d2267603d6e1cee6a55664516736008167d5881d
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-10-01 17:00:42 +0300

    build teacher page

--- src/Layouts/Admin/TeacherUser.jsx
+++ src/Layouts/Admin/TeacherUser.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { TeachersPage } from '../../Pages/AllPages'
+import TitleHeader from '../../Components/TitleHeader'
 
 const TeacherUser = () => {
   return (
-    <div>TeacherUser</div>
+    <>
+      <div className="flex flex-col items-center gap-y-2">
+        <TitleHeader text={"Teachers"} spaceBottom={0} />
+        <TeachersPage />
+      </div>
+    </>
   )
 }
 
