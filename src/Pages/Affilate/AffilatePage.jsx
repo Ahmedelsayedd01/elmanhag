5c0a1df4edd6668c5d1c0379df1c5560442d1ddf
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-10-03 16:15:18 +0300

    add link andriod app

--- src/Pages/Affilate/AffilatePage.jsx
+++ src/Pages/Affilate/AffilatePage.jsx
@@ -11,7 +11,7 @@ const AffilatePage = () => {
        }
        return (
               <>
-       <div className="w-full flex flex-col gap-y-5 p-3">
+                     <div className="w-full flex flex-col gap-y-5 p-3">
                             {/* <div className='flex items-center gap-5'>
                                    <h1 className='w-52'>Download Android App : </h1>
                                    <div className="flex items-center justify-center w-100">
@@ -45,23 +45,26 @@ const AffilatePage = () => {
                                           />
                                    </div>
                             </div> */}
-             <div className="mt-12 text-center">
-                <h1 className="text-2xl sm:text-4xl font-bold mb-8 text-mainColor">حمل التطبيق الان</h1>
-                <div className="flex flex-wrap justify-center gap-8">
-                    {/* Android Download */}
-                    <div className="w-36 sm:w-44 h-36 sm:h-44 bg-black rounded-full shadow-lg flex flex-col items-center justify-center hover:scale-110 transform transition duration-300 ease-in-out cursor-pointer">
-                        <DiAndroid className="text-5xl sm:text-6xl text-white" />
-                        <span className="block mt-3 text-white text-lg sm:text-xl">Android</span>
-                    </div>
+                            <div className="mt-12 text-center">
+                                   <h1 className="text-2xl sm:text-4xl font-bold mb-8 text-mainColor">حمل التطبيق الان</h1>
+                                   <div className="flex flex-wrap justify-center gap-8">
+                                          {/* Android Download */}
+                                          <div
+                                                 onClick={() => window.open('https://play.google.com/store/apps/details?id=com.elmanhag.aff', '_blank', 'noopener,noreferrer')}
+                                                 className="w-36 sm:w-44 h-36 sm:h-44 bg-black rounded-full shadow-lg flex flex-col items-center justify-center hover:scale-110 transform transition duration-300 ease-in-out cursor-pointer"
+                                          >
+                                                 <DiAndroid className="text-5xl sm:text-6xl text-white" />
+                                                 <span className="block mt-3 text-white text-lg sm:text-xl">Android</span>
+                                          </div>
 
-                    {/* iOS Download */}
-                    <div className="w-36 sm:w-44 h-36 sm:h-44 bg-black rounded-full shadow-lg flex flex-col items-center justify-center hover:scale-110 transform transition duration-300 ease-in-out cursor-pointer">
-                        <DiApple className="text-5xl sm:text-6xl text-white" />
-                        <span className="block mt-3 text-white text-lg sm:text-xl">iOS</span>
-                    </div>
-                </div>
-              </div>
-       </div>
+                                          {/* iOS Download */}
+                                          <div className="w-36 sm:w-44 h-36 sm:h-44 bg-black rounded-full shadow-lg flex flex-col items-center justify-center hover:scale-110 transform transition duration-300 ease-in-out cursor-pointer">
+                                                 <DiApple className="text-5xl sm:text-6xl text-white" />
+                                                 <span className="block mt-3 text-white text-lg sm:text-xl">iOS</span>
+                                          </div>
+                                   </div>
+                            </div>
+                     </div>
               </>
        )
 }
--- src/Pages/Teacher/TecherPage.jsx
+++ src/Pages/Teacher/TecherPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { DiAndroid, DiApple } from 'react-icons/di'
+import { NavLink } from 'react-router-dom'
 
 const TecherPage = () => {
        return (
@@ -9,10 +10,14 @@ const TecherPage = () => {
                                    <h1 className="text-2xl sm:text-4xl font-bold mb-8 text-mainColor">حمل التطبيق الان</h1>
                                    <div className="flex flex-wrap justify-center gap-8">
                                           {/* Android Download */}
-                                          <div className="w-36 sm:w-44 h-36 sm:h-44 bg-black rounded-full shadow-lg flex flex-col items-center justify-center hover:scale-110 transform transition duration-300 ease-in-out cursor-pointer">
+                                          <div
+                                                 onClick={() => window.open('https://play.google.com/store/apps/details?id=com.elmanhag.aff', '_blank', 'noopener,noreferrer')}
+                                                 className="w-36 sm:w-44 h-36 sm:h-44 bg-black rounded-full shadow-lg flex flex-col items-center justify-center hover:scale-110 transform transition duration-300 ease-in-out cursor-pointer"
+                                          >
                                                  <DiAndroid className="text-5xl sm:text-6xl text-white" />
                                                  <span className="block mt-3 text-white text-lg sm:text-xl">Android</span>
                                           </div>
+
 
                                           {/* iOS Download */}
                                           <div className="w-36 sm:w-44 h-36 sm:h-44 bg-black rounded-full shadow-lg flex flex-col items-center justify-center hover:scale-110 transform transition duration-300 ease-in-out cursor-pointer">
