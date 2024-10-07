57be4eeaf8add477728f1773081704bd54c94e44
basantsaed2 <basantsaed718@gmail.com>
2024-10-07 15:03:29 +0300

    fcommit

--- src/Components/SidebarStudent.jsx
+++ src/Components/SidebarStudent.jsx
@@ -23,7 +23,7 @@ import MenuSide from './MenuSide'; // Adjust the import based on your file struc
 const SidebarStudent = ({ isOpen }) => {
     return (
         <aside
-            className={`fixed lg:w-1/5 sm:w-4/5 z-10 h-screen right-0 overflow-hidden flex flex-col items-center bg-mainColor gap-y-3 transition-transform transform ${
+            className={`scrollSec fixed lg:w-1/5 sm:w-4/5 z-10 h-screen right-0 overflow-y-auto flex flex-col items-center bg-mainColor gap-y-3 transition-transform transform ${
                 isOpen ? "translate-x-0" : "translate-x-full"
             } lg:translate-x-0`}
         >
