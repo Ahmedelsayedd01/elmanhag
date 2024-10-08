2378fda8b4c3257a5f955ec086e9b1b60f6f1dff
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-10-08 14:45:41 +0300

    customize layout

--- src/Layouts/Admin/LiveLayout.jsx
+++ src/Layouts/Admin/LiveLayout.jsx
@@ -5,11 +5,11 @@ import TitleHeader from '../../Components/TitleHeader'
 const LiveLayout = () => {
        return (
               <>
-              <div className="flex flex-col items-center gap-y-4">
-                     <TitleHeader text={"Live"} spaceBottom={3} />
-                     <LivePage />
-              </div>
-              </> 
+                     <div className="flex flex-col items-center gap-y-4">
+                            <TitleHeader text={"Live"} spaceBottom={3} />
+                            <LivePage />
+                     </div>
+              </>
        )
 }
 
