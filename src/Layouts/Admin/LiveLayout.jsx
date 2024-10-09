9b3b38f37456559870bae2fe9edbaa3e4e1d425f
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-10-09 16:44:07 +0300

    customize layout

--- src/Layouts/Admin/LiveLayout.jsx
+++ src/Layouts/Admin/LiveLayout.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import TitleHeader from '../../Components/TitleHeader'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { LiveNavPage } from '../../Pages/AllPages'
 
 const LiveLayout = () => {
+       const location = useLocation();
+       console.log('location', location);
+
        return (
               <>
                      <div className="flex flex-col items-center gap-y-4">
-                            <TitleHeader text={"Live"} spaceBottom={0} />
-                            {/* <LivePage /> */}
-                            <LiveNavPage />
+                            {(location.pathname === '/dashboard_admin/live/upcoming' || location.pathname === '/dashboard_admin/live/history') && (
+                                   <>
+                                          <TitleHeader text={"Live"} spaceBottom={0} />
+                                          <LiveNavPage />
+                                   </>
+                            )}
                             <Outlet />
                      </div>
               </>
-       )
+       );
 }
 
-export default LiveLayout
-
-
+export default LiveLayout;
