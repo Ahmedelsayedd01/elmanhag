94145b28b07f2acb035a16e95f5c5626170219b1
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-10-16 16:48:06 +0300

    change edit student

--- src/Layouts/Admin/EditeProfileStudent.jsx
+++ src/Layouts/Admin/EditeProfileStudent.jsx
@@ -5,35 +5,35 @@ import { useNavigate, useParams } from 'react-router-dom';
 import HeaderPageSection from '../../Components/HeaderPageSection';
 
 // Create the context outside the component
-export const StudentDataContext = createContext();
+// export const StudentDataContext = createContext();
 
 const EditeProfileStudent = () => {
     const [allStudent, setAllStudent] = useState([]);
     const [studentEdit, setStudentEdit] = useState(null);
-    const { profileId } = useParams();
+    // const { profileStudentId } = useParams();
     const navigate = useNavigate();
 
-    useEffect(() => {
-        const allStudentData = () => {
-            const allStudent = JSON.parse(localStorage.getItem('students'));
-            if (allStudent && allStudent.students) {
-                setAllStudent(allStudent.students);
-            }
-        };
-        allStudentData();
-    }, []);
+    // useEffect(() => {
+    //     const allStudentData = () => {
+    //         const allStudent = JSON.parse(localStorage.getItem('students'));
+    //         if (allStudent && allStudent.students) {
+    //             setAllStudent(allStudent.students);
+    //         }
+    //     };
+    //     allStudentData();
+    // }, []);
 
-    useEffect(() => {
-        if (allStudent.length > 0 && profileId) {
-            const filteredStudent = allStudent.find(
-                (student) => student.id === parseInt(profileId)
-            );
-            setStudentEdit(filteredStudent);
-        }
-    }, [allStudent, profileId]);
+    // useEffect(() => {
+    //     if (allStudent.length > 0 && profileStudentId) {
+    //         const filteredStudent = allStudent.find(
+    //             (student) => student.id === parseInt(profileStudentId)
+    //         );
+    //         setStudentEdit(filteredStudent);
+    //     }
+    // }, [allStudent, profileStudentId]);
 
-    console.log('allStudent', allStudent);
-    console.log('studentEdit', studentEdit);
+    // console.log('allStudent', allStudent);
+    // console.log('studentEdit', studentEdit);
 
     const handleGoBack = () => {
         navigate('/dashboard_admin/student', { replace: true });
@@ -41,18 +41,18 @@ const EditeProfileStudent = () => {
 
     return (
         <>
-            <HeaderPageSection name='Edit' handleClick={handleGoBack} />
+            <HeaderPageSection name='Edit Student' handleClick={handleGoBack} />
             <div className="LinksProfile flex flex-wrap w-full justify-between mb-8 pt-5">
                 <Taps path={'profile'} name={"profile"} />
                 <Taps path={'parent'} name={"parent"} />
                 <Taps path={'Purchases'} name={"Purchases"} />
                 <Taps path={'Progress'} name={"Progress"} />
-                <Taps path={'loginHistory'} name={"loginHistory"} />
+                <Taps path={'loginHistory'} name={"login History"} />
             </div>
-            <h1>Profile ID: {profileId}</h1>
-            <StudentDataContext.Provider value={studentEdit}>
-                <EditProfilePage />
-            </StudentDataContext.Provider>
+            {/* <h1>Profile ID: {profileStudentId}</h1> */}
+            {/* <StudentDataContext.Provider value={studentEdit}> */}
+            <EditProfilePage />
+            {/* </StudentDataContext.Provider> */}
         </>
     );
 };
